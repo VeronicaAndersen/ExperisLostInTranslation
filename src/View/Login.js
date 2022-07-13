70bd62e5e0c76e11bb54096fae18d3df96ebd75b
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-07-13 14:04:03 +0200

    errors with routes

--- src/View/Login.js
+++ src/View/Login.js
@@ -1,8 +1,10 @@
+import LoginForm from "../Components/Login/LoginForm";
+
 const Login = () => {
 
     return (
         <>
-            <h1> Login </h1>
+        <LoginForm/>
         </>
     )
 }
