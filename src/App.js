e88cfcdc0a7dffd25962ca328f2bc1d4f6981307
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-13 13:19:23 +0200

    Added Login, Transaltion & Profile 'pages'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
+import Login from './View/Login';
+import Translation from './View/Translation';
+import Profile from './View/Profile';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <header className = "header">
         <h1>Lost in Translation</h1>
       </header>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/translation' element={<Translation />}/>
+        <Route path='/profile' element={<Profile />}/>
+      </Routes>
     </div>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ src/View/Translation.js
@@ -0,0 +1,10 @@
+const Translation = () => {
+
+    return (
+        <>
+            <h1> Translation </h1>
+        </>
+    )
+}
+
+export default Translation;
