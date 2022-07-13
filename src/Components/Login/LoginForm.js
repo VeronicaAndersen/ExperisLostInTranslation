7472061384ed42436d5c2b6925858baa5661042c
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-07-13 14:40:32 +0200

    Feat: login form

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,15 +1,49 @@
+import { useForm} from 'react-hook-form'
+
+const usernameConfig = {
+     required: true,
+     minLength: 3
+}
+
 const LoginForm = () => {
 
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm()
+
+    const onSubmit = (data) => {
+    }
+
+    const errorMessage = (() => {
+        if (!errors.username){
+            return null
+        }
+
+        if (errors.username.type === "required") {
+        return <span>Username is required</span>
+        }
+
+        if (errors.username.type === "minLength") {
+        return <span>Username is too short. Requires a minimum of three characters.</span>
+        }
+    }) ()
 
 return (
     <>
-    <form id="login-form">
-        <label>Text here</label>
-    <input type="text"/>
+    <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
+        <fieldset>
+        <label htmlFor="username">Username:</label>
+        <input type="text" placeholder="Username" {...register("username", usernameConfig)}/>
+        {errorMessage}
+        </fieldset>
+
+
+        <button type="submit">Continue</button>
     </form>
     </>
 )
 
 }
-
 export default LoginForm;
