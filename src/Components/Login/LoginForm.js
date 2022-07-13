6f7a20c618523bbacae4597e0a143020ec244542
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-13 14:57:59 +0200

    Added styling on LoginForm

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,8 +1,9 @@
-import { useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
+import { VscArrowRight } from "react-icons/vsc";
 
 const usernameConfig = {
-     required: true,
-     minLength: 3
+    required: true,
+    minLength: 3
 }
 
 const LoginForm = () => {
@@ -10,40 +11,38 @@ const LoginForm = () => {
     const {
         register,
         handleSubmit,
-        formState: {errors}
+        formState: { errors }
     } = useForm()
 
     const onSubmit = (data) => {
     }
 
     const errorMessage = (() => {
-        if (!errors.username){
+        if (!errors.username) {
             return null
         }
 
         if (errors.username.type === "required") {
-        return <span>Username is required</span>
+            return <span>Username is required</span>
         }
 
         if (errors.username.type === "minLength") {
-        return <span>Username is too short. Requires a minimum of three characters.</span>
+            return <span>Username is too short. Requires a minimum of three characters.</span>
         }
-    }) ()
+    })()
 
-return (
-    <>
-    <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
-        <fieldset>
-        <label htmlFor="username">Username:</label>
-        <input type="text" placeholder="Username" {...register("username", usernameConfig)}/>
-        {errorMessage}
-        </fieldset>
-
-
-        <button type="submit">Continue</button>
-    </form>
-    </>
-)
+    return (
+        <>
+            <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
+                <fieldset>
+                    <label htmlFor="username">Username:</label>
+                    <input type="text" placeholder="Username" {...register("username", usernameConfig)} />
+                    <button type="submit"><VscArrowRight /></button>
+                    {errorMessage}
+                </fieldset>
+            </form>
+        </>
+    )
 
 }
 export default LoginForm;
