88e3d4342749930c793ede492a1fb00bd2550f21
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-13 11:22:01 +0200

    Added styling for background & header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header class= "header">Lost in Translation</header>
+      <header class= "header">
+        <h1>Lost in Translation</h1>
+      </header>
     </div>
   );
 }
