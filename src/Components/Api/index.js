01fd8634800a1befb3e10cf162530518907821d2
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-13 18:06:01 +0200

    Login implementation api

---
+++ src/Components/Api/index.js
@@ -0,0 +1,8 @@
+const apiKEY = process.env.REACT_APP_API_KEY
+
+export const createHeaders = () => {
+    return {
+        'Content-Type': 'application/json',
+        'x-api-key': apiKEY
+    }
+}
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,45 +1,56 @@
+import { useState } from 'react';
 import { useForm } from 'react-hook-form'
 import { VscArrowRight } from "react-icons/vsc";
+import { LoginUser } from '../Api/user';
 
 const usernameConfig = {
     required: true,
-    minLength: 3
-}
-
+    minLength: 3,
+};
 const LoginForm = () => {
-
     const {
         register,
         handleSubmit,
         formState: { errors }
     } = useForm()
 
-    const onSubmit = (data) => {
+    const [ loading, setLoading ] = useState(false);
+
+    const onSubmit = async ({username}) => {
+        setLoading(true);
+        const [error, user] = await LoginUser(username);
+        console.log('Error: ', error);
+        console.log('User: ', user);
+        setLoading(false);
     }
+    console.log(errors);
 
     const errorMessage = (() => {
         if (!errors.username) {
             return null
         }
-
-        if (errors.username.type === "required") {
-            return <span className='validateLogin'> *Username is required</span>
+        if (errors.username.type === 'required') {
+            return <span className='validateLogin'>*Username is required</span>
         }
-
-        if (errors.username.type === "minLength") {
-            return <span className='validateLogin'> *Username is too short. Requires a minimum of three characters.</span>
+        if (errors.username.type === 'minLength') {
+            return <span className='validateLogin'>*Username is too short [min. 3]</span>
         }
     })()
 
     return (
         <>
-            <form id="login-form" onSubmit={handleSubmit(onSubmit)}>
+
+
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <fieldset>
-                    <label htmlFor="username">Username:</label>
-                    <input type="text" placeholder="Username" {...register("username", usernameConfig)} />
-                    <button type="submit"><VscArrowRight /></button>
-                    {errorMessage}
+                    <input
+                        placeholder='What´s your name?'
+                        type="text" {...register("username", usernameConfig)} />
+
+                    <button type='submit' disabled={ loading }><VscArrowRight /></button>
                 </fieldset>
+                { loading && <p>Logging in...</p>}
+                { errorMessage }
             </form>
         </>
     )
