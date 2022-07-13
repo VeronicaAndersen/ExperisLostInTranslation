96bc19cfb812b123343f6b7cd592089cacdd06ba
Veronica Andersen <veronica.andersen@se.experis.com>
2022-07-13 12:48:42 +0200

    Fixed error in App.js from class to className

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header class= "header">
+      <header className = "header">
         <h1>Lost in Translation</h1>
       </header>
     </div>
