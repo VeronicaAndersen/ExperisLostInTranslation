04f37a285e459cccf29b8cffb4e8a4f6999b04d1
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-07-14 14:08:36 +0200

    feat: local storage

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,31 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form'
 import { VscArrowRight } from "react-icons/vsc";
 import { LoginUser } from '../Api/user';
-import {BiLoaderCircle} from 'react-icons/bi'
+import { BiLoaderCircle } from 'react-icons/bi'
+import { StorageSave } from '../../Utils/Storage';
+//import { useHistory } from 'react-router-dom';
 
 const usernameConfig = {
     required: true,
     minLength: 3,
 };
 const LoginForm = () => {
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm()
+    const { register, handleSubmit, formState: { errors } } = useForm()
 
+    //Local state
     const [loading, setLoading] = useState(false);
+    const [apiError, setApiError] = useState(null);
 
+    //Side effects
+    useEffect(() => {
+        // if (user) {
+        //     //redirect
+        // }
+    }, [] ) //Empty dependencies, will only run once
+
+
+
+    //Event handlers
     const onSubmit = async ({ username }) => {
         setLoading(true);
         const [error, user] = await LoginUser(username);
-        console.log('Error: ', error);
-        console.log('User: ', user);
+        if (error !== null) {
+            setApiError(error)
+        }
+        if (user !== null) {
+            StorageSave("translate-user", user)
+        }
         setLoading(false);
-    }
-    console.log(errors);
 
+    }
+
+    //Render functions
     const errorMessage = (() => {
         if (!errors.username) {
             return null
@@ -49,8 +64,9 @@ const LoginForm = () => {
                         type="text" {...register("username", usernameConfig)} />
 
                     <button type='submit' disabled={loading}><VscArrowRight /></button>
-                    {loading && <h2>Logging in <BiLoaderCircle/> </h2>}
+                    {loading && <h2>Logging in <BiLoaderCircle /> </h2>}
                     {errorMessage}
+                    {apiError && <p>{apiError}</p>}
                 </fieldset>
             </form>
         </>
---
+++ src/Components/Translation/TranslatePicture.js
@@ -0,0 +1,11 @@
+// import Picture from "./../../signs/a.png";
+
+// const Translate = () => {
+
+//     for (let i = 0; i < 5; i++) {
+//         //show sprite with same name as stringToTranslate[i]
+//         return (<img src={Picture} alt="Heyy"></img>)
+//     }
+// }
+
+// export default Translate();
