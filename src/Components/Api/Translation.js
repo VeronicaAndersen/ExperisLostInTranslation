71ff51cffe2b171bd2be3343c01cfdf7ad252cb7
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-07-18 11:16:34 +0000

    Revert "Merge branch 'revert-2b8c1269' into 'side'"
    This reverts merge request !2

--- src/Components/Api/Translation.js
+++ src/Components/Api/Translation.js
@@ -6,9 +6,13 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 export const addTranslation = async (translationAdded) => {
     const user = storageRead(STORAGE_KEY_USER)
-let translations = user.translations;
-    
-    translations.push(translationAdded)
+    let translations = user.translations;
+
+    if (translations.length >= 10) {
+        translations.splice(0, 1);
+    }
+    translations.push(translationAdded);
+
     user.translations = translations
     StorageSave(STORAGE_KEY_USER, user)
 
@@ -21,13 +25,13 @@ let translations = user.translations;
             })
 
         })
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error("Could not update translation")
         }
         const result = await response.JSON
         return [null, result]
     }
-    catch(error) {
+    catch (error) {
         return [error.message, null]
     }
 
@@ -35,24 +39,24 @@ let translations = user.translations;
 
 export const translationClearHistory = async (userId) => {
     try {
-        const response = await fetch(`${apiUrl}/${userId}`,{
+        const response = await fetch(`${apiUrl}/${userId}`, {
             method: "PATCH",
             headers: createHeaders(),
-            body:JSON.stringify({
+            body: JSON.stringify({
                 translations: []
             })
-        })   
+        })
         if (!response.ok) {
             throw new Error('Couldn´t update translations');
         }
         const result = await response.json();
 
-        return[null, result];
+        return [null, result];
 
     } catch (error) {
 
         return [error.message, null];
 
     }
-    
+
 }
