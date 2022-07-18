a9ed6f5a1653f0abaaea2cf38737022c511ec7b7
Adrian Mattsson <adrian.mattsson@se.experis.com>
2022-07-18 11:14:53 +0000

    Revert "perf: Removed unneccessary functions"
    This reverts commit 2b8c126936b0e0fe49a0e4e3585aec2bbc379c7b

--- src/Components/Translation/TranslationInput.js
+++ src/Components/Translation/TranslationInput.js
@@ -2,54 +2,63 @@ import { useState } from 'react';
 import { VscArrowRight } from 'react-icons/vsc'
 import { useForm } from 'react-hook-form'
 import { addTranslation } from '../Api/Translation';
+import { useUser } from '../../context/UserContext';
 
 const usernameConfig = {
     maxLength: 40,
 };
 
 const TranslationSearch = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm(); 
+    const {user} = useUser();                                               
+
     const [imageList, setImageList] = useState([]);
+
     const onSubmit = ({ string }) => {
-        Translate(string);
+        Translate(string); 
+    }   
+    
+    const handleTranslationSave = async (notes) => {
+
+        const [error, result] = await addTranslation(notes)
     }
 
     const Translate = (string) => {
 
         let imageArray = [];
 
-        if (!/^[a-zA-Z ]*$/g.test(string)) {
+        if (!/^[a-zA-Z ]*$/g.test(string)){
             alert("Translation denied. Only letters ranging a-z are currently supported.")
             return;
         }
 
-        addTranslation(string);
+        handleTranslationSave(string);
         string = string.toLowerCase();
 
         for (let i = 0; i < string.length; i++) {
-            if (string[i] !== " ") {
+            if(string[i] !== " ") {
                 imageArray.push(<img className="sign-img" src={"signs/" + string[i] + ".png"} alt={string[i]} key={i} />);
             }
         }
-        setImageList(imageArray);
+            setImageList(imageArray);
     }
 
     return (<>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <fieldset>
-                <input
-                    placeholder='Translate something'
-                    type="text" {...register("string", usernameConfig)} />
+        <fieldset>
+            <input
+                placeholder='Translate something'
+                type="text" {...register("string", usernameConfig)} />
 
-                <button type='submit'><VscArrowRight /></button>
+            <button type='submit'><VscArrowRight /></button>
                 <div className="TranslationSquare">
 
                     <div>{imageList}</div>
 
-                    <div id="translation">Translation</div>
-                </div>
+             <div id="translation">Translation</div>
+         </div>
 
-            </fieldset>
+        </fieldset>
         </form>
     </>)
 }
